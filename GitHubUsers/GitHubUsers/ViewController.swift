1505f825131dd502a02945de9b2f006da6d6616f
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-18 16:48:40 -0300

    initial commit

---
+++ GitHubUsers/GitHubUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUsers
+//
+//  Created by Lucas Portella on 2025/04/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHubUsers/GitHubUsersTests/GitHubUsersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersTests.swift
+//  GitHubUsersTests
+//
+//  Created by Lucas Portella on 2025/04/18.
+//
+
+import Testing
+@testable import GitHubUsers
+
+struct GitHubUsersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
