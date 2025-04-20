33cc75425a6f433bdbc83c38b5898e6de3e09819
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-20 00:12:03 -0300

    add nslog to the request

--- GitHubUsers/NetworkKit/Sources/NetworkKit/Core/CoreNetworking.swift
+++ GitHubUsers/NetworkKit/Sources/NetworkKit/Core/CoreNetworking.swift
@@ -37,6 +37,7 @@ class CoreNetworking {
         do {
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
+            NSLog(String(data: data, encoding: .utf8) ?? "Cannot convert data")
             return try decoder.decode(T.self, from: data)
         } catch {
             throw ApiError.decodingError(error, data: data)
