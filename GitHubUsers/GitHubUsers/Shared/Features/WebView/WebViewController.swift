304813de90c1867016133689eaed69b6cac1e7f2
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-20 17:22:34 -0300

    add webview constraints, refresh and loading; also added shared view protocols

--- GitHubUsers/GitHubUsers/Shared/Features/WebView/WebViewController.swift
+++ GitHubUsers/GitHubUsers/Shared/Features/WebView/WebViewController.swift
@@ -8,9 +8,29 @@
 import UIKit
 import WebKit
 
-final class WebViewController: UIViewController, WKNavigationDelegate {
+final class WebViewController: UIViewController, WKNavigationDelegate, ViewBuilding {
     private let url: URL
-    private let webView = WKWebView()
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        return activityIndicator
+    }()
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshWebView), for: .valueChanged)
+        return refreshControl
+    }()
+    
+    lazy var webView: WKWebView = {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.scrollView.refreshControl = refreshControl
+        return webView
+    }()
     
     init(url: URL) {
         self.url = url
@@ -19,5 +39,35 @@ final class WebViewController: UIViewController, WKNavigationDelegate {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewBuilding()
+    }
+}
+
+extension WebViewController {
+    func setupViews() {
+        view.addSubview(webView)
+        view.addSubview(activityIndicator)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+private extension WebViewController {
+    @objc func refreshWebView() {
+        webView.reload()
     }
 }
