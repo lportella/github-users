051923b43caaaf5919517b52b63e1d0f624786b8
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-20 14:53:01 -0300

    add username property to make request

--- GitHubUsers/GitHubUsers/UserDetails/UserDetailsViewModel.swift
+++ GitHubUsers/GitHubUsers/UserDetails/UserDetailsViewModel.swift
@@ -10,10 +10,29 @@ import NetworkKit
 final class UserDetailsViewModel: UserDetailsUseCase, RequestUseCase {
     var onLoadingChanged: ((Bool) -> Void)?
     var onError: (([ApiError]) -> Void)?
+    
+    let username: String
+    
+    init(username: String) {
+        self.username = username
+    }
 }
 
 extension UserDetailsViewModel {
     func fetchUserDetails() async {
-        // MARK: To do
+        do {
+            await MainActor.run { [weak self] in
+                self?.onLoadingChanged?(true)
+            }
+            
+            // MARK: add async request for user details and repos
+            
+            await MainActor.run { [weak self] in
+                // MARK: add use case for user details
+                self?.onLoadingChanged?(false)
+            }
+        } catch {
+            print("Error: \(error)")
+        }
     }
 }
