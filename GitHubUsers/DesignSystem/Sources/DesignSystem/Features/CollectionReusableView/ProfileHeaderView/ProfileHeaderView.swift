d86a2101f924f20fe2a0675c8c4f9481df2ba71d
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-21 20:31:40 -0300

    add profile header view

---
+++ GitHubUsers/DesignSystem/Sources/DesignSystem/Features/CollectionReusableView/ProfileHeaderView/ProfileHeaderView.swift
@@ -0,0 +1,71 @@
+//
+//  ProfileHeaderView.swift
+//  DesignSystem
+//
+//  Created by Lucas Portella on 2025/04/21.
+//
+
+import UIKit
+import SDWebImage
+
+public final class ProfileHeaderView: UICollectionReusableView {
+    static let reuseIdentifier = "HeaderView"
+    
+    lazy var avatar: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = ImageSize.large.cornerRadius
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var username: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: TextSize.medium.value, weight: .bold)
+        label.textColor = UIColor(named: CustomColors.primaryBlack.name)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var name: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: TextSize.small.value, weight: .regular)
+        label.textColor = UIColor(cgColor: .init(red: 38/255, green: 40/255, blue: 46/255, alpha: 1))
+        // MARK: Testing colors! Move it to Colors.xcassets after validating.
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var followers: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: TextSize.medium.value, weight: .regular)
+        label.textColor = UIColor(named: CustomColors.primaryBlack.name)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var following: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: TextSize.medium.value, weight: .regular)
+        label.textColor = UIColor(named: CustomColors.primaryBlack.name)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 12
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+}
