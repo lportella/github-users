0119e78007519ca66a88072614312f3abc5934f6
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-21 00:02:07 -0300

    fix single expected result and added user list view model tests

---
+++ GitHubUsers/GitHubUsersTests/Features/UserList/UserListViewModelTests.swift
@@ -0,0 +1,65 @@
+//
+//  UserListViewModelTests.swift
+//  GitHubUsers
+//
+//  Created by Lucas Portella on 2025/04/20.
+//
+
+import Foundation
+import Testing
+
+@testable import NetworkKit
+@testable import GitHubUsers
+
+struct UserListViewModelTests {
+    
+    @Test
+    func testFetchUserList_WhenSuccess_ShouldReturnListOfUsers() async throws {
+        let args = makeSUT()
+        let expectedUserList = BaseUser.baseUserFixture
+        
+        args.networkMock.expectedResult = .success(expectedUserList)
+        
+        await args.sut.fetchUserList()
+        
+        #expect(args.viewControllerSpy.messages == [
+            .handleLoading(true),
+            .handleUserList(expectedUserList),
+            .handleLoading(false)
+        ])
+    }
+    
+    @Test
+    func testDidSelectUser_WhenUserSelected_ShouldPassUserLoginToNavigation() async throws {
+        let args = makeSUT()
+        let expectedUserList = BaseUser.baseUserFixture
+        let expectedUser = try #require(expectedUserList.first)
+        
+        args.sut.didSelectUser(expectedUser)
+        
+        #expect(args.coordinatorSpy.messages == [.didSelectUser(expectedUser.login)])
+    }
+    
+}
+
+private extension UserListViewModelTests {
+    typealias SUT = (
+        sut: UserListUseCase,
+        networkMock: NetworkExposableMock,
+        viewControllerSpy: UserListViewControllerSpy,
+        coordinatorSpy: UserListCoordinatorSpy
+    )
+    
+    func makeSUT() -> SUT {
+        let networkMock = NetworkExposableMock()
+        let viewControllerSpy = UserListViewControllerSpy()
+        let coordinatorSpy = UserListCoordinatorSpy()
+        let viewModel = UserListViewModel(networkService: networkMock)
+        
+        viewModel.onLoadingChanged = viewControllerSpy.handleLoading(_:)
+        viewModel.onUserListLoaded = viewControllerSpy.handleUserList(_:)
+        viewModel.navigationHandler = coordinatorSpy
+        
+        return (viewModel, networkMock, viewControllerSpy, coordinatorSpy)
+    }
+}
