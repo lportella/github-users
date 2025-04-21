61a0296fd2c0d2a0734bed0ac961eacac068ebca
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-21 18:26:40 -0300

    added resources with color assets in DesignSystem; also added cell components

--- GitHubUsers/DesignSystem/Package.swift
+++ GitHubUsers/DesignSystem/Package.swift
@@ -9,16 +9,17 @@ let package = Package(
         .iOS(.v16)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DesignSystem",
             targets: ["DesignSystem"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DesignSystem"),
+            name: "DesignSystem",
+            resources: [
+                .process("Resources/Colors.xcassets")
+            ]
+        ),
         .testTarget(
             name: "DesignSystemTests",
             dependencies: ["DesignSystem"]
--- GitHubUsers/DesignSystem/Sources/DesignSystem/Features/Cells/SimpleCardViewCell.swift
+++ GitHubUsers/DesignSystem/Sources/DesignSystem/Features/Cells/SimpleCardViewCell.swift
@@ -8,6 +8,33 @@
 import UIKit
 
 /// This reusable cell is a simple cell displaying:
-/// -    one centered image
-/// -    one centered text
-class SimpleCardViewCell: UICollectionViewCell {}
+/// -    imageView: one centered image
+/// -    text: a label centered below the image
+class SimpleCardViewCell: UICollectionViewCell {
+    lazy var containerView: UIView = {
+        let container = UIView()
+        container.layer.cornerRadius = 14
+        container.backgroundColor = UIColor(red: 240/255.0, green: 241/255.0, blue: 246/255.0, alpha: 1.0)
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 24
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var text: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textColor = UIColor(red: 17/255.0, green: 20/255.0, blue: 31/255.0, alpha: 1.0)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+}
