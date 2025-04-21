828d592783e56ade7e703b01c3128634d72f178b
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-20 23:49:07 -0300

    add user list mock

---
+++ GitHubUsers/GitHubUsersTests/Features/UserList/Model/UserListMock.swift
@@ -0,0 +1,37 @@
+//
+//  UserListMock.swift
+//  GitHubUsers
+//
+//  Created by Lucas Portella on 2025/04/20.
+//
+
+import Foundation
+
+@testable import GitHubUsers
+
+extension BaseUser {
+    static var baseUserfakeJSON: String {
+        """
+            [
+              {
+                "login": "octocat",
+                "id": 1,
+                "avatar_url": "https://github.com/images/error/octocat_happy.gif"
+              }
+            ]
+        """
+    }
+    
+    static var baseUserFixture: [BaseUser] {
+        do {
+            guard let data = baseUserfakeJSON.data(using: .utf8) else {
+                fatalError("Could not convert fake JSON to data")
+            }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode([Self].self, from: data)
+        } catch {
+            fatalError("Failed to decode with fake JSON. \(error)")
+        }
+    }
+}
