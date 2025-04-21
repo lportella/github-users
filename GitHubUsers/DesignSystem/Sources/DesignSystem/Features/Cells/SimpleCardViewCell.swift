654576226dd5bd9380a10934c8a7fdd95c3d14cf
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-21 18:07:09 -0300

    added simple card view class

---
+++ GitHubUsers/DesignSystem/Sources/DesignSystem/Features/Cells/SimpleCardViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SimpleCardViewCell.swift
+//  DesignSystem
+//
+//  Created by Lucas Portella on 2025/04/21.
+//
+
+import UIKit
+
+/// This reusable cell is a simple cell displaying:
+/// -    one centered image
+/// -    one centered text
+class SimpleCardViewCell: UICollectionViewCell {}
