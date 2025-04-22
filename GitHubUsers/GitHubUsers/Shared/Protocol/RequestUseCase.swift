e75232c55a5ee78c1aff9b6444f58f92de1c4a55
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-22 18:53:07 -0300

    remove on error unused var

--- GitHubUsers/GitHubUsers/Shared/Protocol/RequestUseCase.swift
+++ GitHubUsers/GitHubUsers/Shared/Protocol/RequestUseCase.swift
@@ -4,9 +4,9 @@
 //
 //  Created by Lucas Portella on 2025/04/19.
 //
+import DesignSystem
 import NetworkKit
 
 protocol RequestUseCase {
     var onLoadingChanged: ((_ isLoading: Bool) -> Void)? { get set }
-    var onError: (([ApiError]) -> Void)? { get set }
 }
