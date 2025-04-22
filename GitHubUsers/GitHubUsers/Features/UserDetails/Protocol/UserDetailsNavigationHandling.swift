624ba0c2749657c04e1bed4052d8fee83924cc01
Lucas Portella <lucas.portellasantos@gmail.com>
2025-04-22 18:51:19 -0300

    added navigation handler for feedback on view model error

--- GitHubUsers/GitHubUsers/Features/UserDetails/Protocol/UserDetailsNavigationHandling.swift
+++ GitHubUsers/GitHubUsers/Features/UserDetails/Protocol/UserDetailsNavigationHandling.swift
@@ -5,8 +5,9 @@
 //  Created by Lucas Portella on 2025/04/20.
 //
 
+import DesignSystem
 import Foundation
 
-protocol UserDetailsNavigationHandling: AnyObject {
+protocol UserDetailsNavigationHandling: AnyObject, FeedbackDisplaying {
     func didSelectRepository(with url: URL)
 }
